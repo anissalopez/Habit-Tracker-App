5694bc06469d3d88cb84d2f828e2c5deaa14b1f9
anissalopez <anissa965@gmail.com>
2023-08-21 16:35:16 -0400

    updated homepage

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { Form, Container, Button, InputGroup  } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 
 function HomePage () {
+
+    const navigate = useNavigate();
+
+    const handleClick = () => navigate('/habits')
     return(
-        <Container className="d-grid align-items-center justify-content-center text-center">
-            <h1>Welcome to Habit Tracker</h1>
-                <Button type="submit" variant="primary" className="mt-3 mb-2">
-                View Weekly Habits
-                </Button>
-    
+        <Container className="d-grid align-items-center justify-content-center text-center mt-5">
+            <h1 className="mt-5">Welcome to Habit Tracker</h1>
+                <div>
+                    <Button onClick={handleClick} type="submit" variant="primary" className="mt-3 mb-2">
+                        View Weekly Habits
+                    </Button>
+                </div>
        </Container>
     )
 
